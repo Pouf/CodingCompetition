c0d3922b38a12e9de4bbca0935b7089e8156cf5c
Benoit Delaunay <delaunay.ben@gmail.com>
2015-10-11 12:23:35 +0200

    Update three-words.py

--- CheckiO/three-words.py
+++ CheckiO/three-words.py
@@ -1,2 +1,8 @@
 def checkio(words):
     return '111' in ''.join('1' if w.isalpha() else '0' for w in words.split())
+
+
+# alt
+from re import findall, I
+def checkio(words):
+    return bool(findall('([a-z]+ +){2,}[a-z]+', words, flags=I))
