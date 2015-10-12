41d6cfc661d45b85ddfe40323b8a2ada86190642
Benoit Delaunay <delaunay.ben@gmail.com>
2015-10-12 11:10:31 +0200

    Update and rename Euler/03.py to E/03.py

--- E/03.py
+++ E/03.py
@@ -1,3 +1,7 @@
+# Problem 03 - Largest prime factor
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
 def prime_sieve(limit):
     is_prime = [False] * (limit + 1)
     for x in range(1,int(limit**.5)+1):
