aff2bc369f1a766a5fb41178b9e9312fc042b51d
Benoit Delaunay <delaunay.ben@gmail.com>
2015-10-17 11:04:37 +0200

    Update digits-multiplication.py

--- CiO/digits-multiplication.py
+++ CiO/digits-multiplication.py
@@ -5,3 +5,7 @@ def checkio(i):
     digits = str(i).replace('0','')
     result = reduce(lambda x, y: int(x)*int(y), digits)
     return int(result)
+
+
+
+golf=lambda n:(n%10or 1)*golf(n//10)if n else 1
