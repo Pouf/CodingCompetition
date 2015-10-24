042a14db3321b66b478c7fac709b6c410a23a19f
Benoit Delaunay <Pouf@users.noreply.github.com>
2015-10-24 15:14:45 +0200

    Create feed-pigeons.py
    initial commit, can do better

---
+++ CiO/feed-pigeons.py
@@ -0,0 +1,12 @@
+def checkio(number):
+    pNew = 0
+    pTotal = 0
+    while number > 0:
+        if number <= pTotal:
+            return pTotal
+        pNew += 1
+        if number < pTotal + pNew:
+            return pTotal + max(number - pTotal, 0)
+        pTotal += pNew
+        number -= pTotal
+    return pTotal
