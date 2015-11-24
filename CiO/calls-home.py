085074e6ba253dc575fea79ca7aec5f834e01c3d
Benoit Delaunay <Pouf@users.noreply.github.com>
2015-11-24 12:45:40 +0100

    Create calls-home.py

---
+++ CiO/calls-home.py
@@ -0,0 +1,8 @@
+from itertools import groupby
+​
+def total_cost(calls):
+    total = 0
+    for k,g in groupby(calls, lambda x:x[:10]):
+        minutes = sum(-(-int(l[20:])//60) for l in g)
+        total += (minutes, 2*minutes-100)[minutes>100]
+    return total
