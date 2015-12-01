31c173bb82b7a60ffae4e67a034863991e96a286
Benoit Delaunay <Pouf@users.noreply.github.com>
2015-12-01 18:09:04 +0100

    Update roman-numerals.py

--- CiO/roman-numerals.py
+++ CiO/roman-numerals.py
@@ -5,8 +5,8 @@ def checkio(n):
     I = n % 10
 
     roman = M * 'M'
-    roman += 'CM'*(C == 9) + 'D'*(4 < C < 9) + 'C'*(C % 9 % 5)
-    roman += 'XC'*(X == 9) + 'L'*(4 < X < 9) + 'X'*(X % 9 % 5)
-    roman += 'IX'*(I == 9) + 'V'*(4 < I < 9) + 'I'*(I % 9 % 5)
+    roman += 'CM'*(C == 9) + 'C'*(C == 4) + 'D'*(3 < C < 9) + 'C'*(C % 9 % 5 % 4)
+    roman += 'XC'*(X == 9) + 'X'*(X == 4) + 'L'*(3 < X < 9) + 'X'*(X % 9 % 5 % 4)
+    roman += 'IX'*(I == 9) + 'I'*(I == 4) + 'V'*(3 < I < 9) + 'I'*(I % 9 % 5 % 4)
 
     return roman
