4e9462bb9366aabf4c014cfbcc1d521786a88b0d
Benoit Delaunay <Pouf@users.noreply.github.com>
2016-10-07 13:33:36 +0200

    Add files via upload

--- Google-Code-Jam/2016-1A/A.py
+++ Google-Code-Jam/2016-1A/A.py
@@ -1,5 +1,4 @@
 import os
-import string
 import sys
 
 
@@ -8,29 +7,31 @@ scriptPath = os.path.dirname(script)
 scriptFile = os.path.basename(script)[0]
 files = [f for f in os.listdir(scriptPath) if scriptFile in f and '.in' in f]
 if '{}-large'.format(scriptFile) in str(files):
-	size = 'large'
+  size = 'large'
 elif '{}-small'.format(scriptFile) in str(files):
-	size = 'small'
+  size = 'small'
 elif '{}-test'.format(scriptFile) in str(files):
-	size = 'test'
+  size = 'test'
 else:
-	print('{}-test not found'.format(scriptFile))
-	sys.exit()
-latest = sorted(f for f in files if size in f)[0][:-3]
+  print('{}-test not found'.format(scriptFile))
+  sys.exit()
+latest = sorted(f for f in files if size in f)[-1][:-3]
 f = '{}/{}'.format(scriptPath, latest)
-I = open(f + '.in', 'r')
-O = open(f + '.out', 'w')
+i = open(f + '.in', 'r')
+o = open(f + '.out', 'w')
 print(f)
-T = int(I.readline())
+T = int(i.readline())
 
+# https://code.google.com/codejam/contest/4304486/dashboard#s=p0
+# Problem A. The Last Word
 
 for x in range(T):
-	S = list(I.readline().rstrip())
-	y = S[0]
-	for s in S[1:]:
-		y = max(y + s, s + y)
-	
-	O.write('{}Case #{}: {}'.format(['', '\n'][x > 0], x+1, y))
+  S = list(i.readline().rstrip())
+  y = S[0]
+  for s in S[1:]:
+    y = max(y + s, s + y)
 
-I.close()
-O.close()
+  o.write('{}Case #{}: {}'.format(['', '\n'][x > 0], x + 1, y))
+
+i.close()
+o.close()
