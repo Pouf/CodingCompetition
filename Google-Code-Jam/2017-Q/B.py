512fb1899b6ddcbbbb5c34e36088d0a673d599a9
Benoit Delaunay <Pouf@users.noreply.github.com>
2018-09-07 13:43:01 +0200

    Update B.py

--- Google-Code-Jam/2017-Q/B.py
+++ Google-Code-Jam/2017-Q/B.py
@@ -3,20 +3,20 @@ import sys
 
 
 script = __file__
-scriptPath = os.path.dirname(script)
-scriptFile = os.path.basename(script)[0]
-files = [f for f in os.listdir(scriptPath) if scriptFile in f and '.in' in f]
-if '{}-large'.format(scriptFile) in str(files):
-  size = 'large'
-elif '{}-small'.format(scriptFile) in str(files):
-  size = 'small'
-elif '{}-test'.format(scriptFile) in str(files):
-  size = 'test'
+script_path = os.path.dirname(script)
+script_file = os.path.basename(script)[0]
+files = [f for f in os.listdir(script_path) if script_file in f and '.in' in f]
+if '{}-large'.format(script_file) in str(files):
+    size = 'large'
+elif '{}-small'.format(script_file) in str(files):
+    size = 'small'
+elif '{}-test'.format(script_file) in str(files):
+    size = 'test'
 else:
-  print('{}-test not found'.format(scriptFile))
-  sys.exit()
+    print('{}-test not found'.format(script_file))
+    sys.exit()
 latest = sorted(f for f in files if size in f)[-1][:-3]
-F = '{}/{}'.format(scriptPath, latest)
+F = '{}/{}'.format(script_path, latest)
 I = open(F + '.in', 'r')
 O = open(F + '.out', 'w')
 print(F)
@@ -27,20 +27,20 @@ T = int(I.readline())  # nb of test cases
 
 
 for x in range(T):
-  N = I.readline().rstrip()
+    N = I.readline().rstrip()
 
-  y = [int(n) for n in N]
-  while sorted(y) != (y):
-    for i in range(len(y) - 1):
-      if y[i] > y[i+1]:
-        y[i] -= 1
-        y[i+1:] = [9] * (len(y) - i - 1)
-        break
-  y = int(''.join(str(n) for n in y))
+    y = [int(n) for n in N]
+    while sorted(y) != (y):
+        for i in range(len(y) - 1):
+            if y[i] > y[i+1]:
+                y[i] -= 1
+                y[i+1:] = [9] * (len(y) - i - 1)
+                break
+    y = int(''.join(str(n) for n in y))
 
-  result = '{}Case #{}: {}'.format('\n' if x else '', x + 1, y)
-  print(result)
-  O.write(result)
+    result = '{}Case #{}: {}'.format('\n' if x else '', x + 1, y)
+    print(result)
+    O.write(result)
 
 I.close()
 O.close()
