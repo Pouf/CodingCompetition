0a90c197ecfcc90e68f47f67fa7c653a3bd16cf4
Benoit Delaunay <Pouf@users.noreply.github.com>
2018-09-07 13:41:46 +0200

    Update D.py

--- Google-Code-Jam/2016-Q/D.py
+++ Google-Code-Jam/2016-Q/D.py
@@ -3,20 +3,20 @@ import sys
 
 
 script = __file__
-scriptPath = os.path.dirname(script)
-scriptFile = os.path.basename(script)[0]
-files = [f for f in os.listdir(scriptPath) if scriptFile in f and '.in' in f]
-if '{}-large'.format(scriptFile) in str(files):
-  size = 'large'
-elif '{}-small'.format(scriptFile) in str(files):
-  size = 'small'
-elif '{}-test'.format(scriptFile) in str(files):
-  size = 'test'
+script_path = os.path.dirname(script)
+script_file = os.path.basename(script)[0]
+files = [f for f in os.listdir(script_path) if script_file in f and '.in' in f]
+if '{}-large'.format(script_file) in str(files):
+    size = 'large'
+elif '{}-small'.format(script_file) in str(files):
+    size = 'small'
+elif '{}-test'.format(script_file) in str(files):
+    size = 'test'
 else:
-  print('{}-test not found'.format(scriptFile))
-  sys.exit()
+    print('{}-test not found'.format(script_file))
+    sys.exit()
 latest = sorted(f for f in files if size in f)[-1][:-3]
-f = '{}/{}'.format(scriptPath, latest)
+f = '{}/{}'.format(script_path, latest)
 i = open(f + '.in', 'r')
 o = open(f + '.out', 'w')
 print(f)
@@ -26,9 +26,9 @@ T = int(i.readline())
 # Problem D. Fractiles
 
 for x in range(T):
-  K, C, S = map(int, i.readline().split())
-  y = ' '.join(str(i + 1) for i in range(S))
-  o.write('{}Case #{}: {}'.format(['', '\n'][x > 0], x + 1, y))
+    K, C, S = map(int, i.readline().split())
+    y = ' '.join(str(i + 1) for i in range(S))
+    o.write('{}Case #{}: {}'.format(['', '\n'][x > 0], x + 1, y))
 
 i.close()
 o.close()
