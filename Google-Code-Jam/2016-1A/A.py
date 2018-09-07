d2c6a4d8915908d3807a1d49e7a11b8df1de52dc
Benoit Delaunay <Pouf@users.noreply.github.com>
2018-09-07 13:26:11 +0200

    Update A.py

--- Google-Code-Jam/2016-1A/A.py
+++ Google-Code-Jam/2016-1A/A.py
@@ -3,20 +3,20 @@ import sys
 
 
 script = __file__
-scriptPath = os.path.dirname(script)
-scriptFile = os.path.basename(script)[0]
-files = [f for f in os.listdir(scriptPath) if scriptFile in f and '.in' in f]
-if '{}-large'.format(scriptFile) in str(files):
+script_path = os.path.dirname(script)
+script_file = os.path.basename(script)[0]
+files = [f for f in os.listdir(script_path) if script_file in f and '.in' in f]
+if '{}-large'.format(script_file) in str(files):
     size = 'large'
-elif '{}-small'.format(scriptFile) in str(files):
+elif '{}-small'.format(script_file) in str(files):
     size = 'small'
-elif '{}-test'.format(scriptFile) in str(files):
+elif '{}-test'.format(script_file) in str(files):
     size = 'test'
 else:
-    print('{}-test not found'.format(scriptFile))
+    print('{}-test not found'.format(script_file))
     sys.exit()
 latest = sorted(f for f in files if size in f)[-1][:-3]
-f = '{}/{}'.format(scriptPath, latest)
+f = '{}/{}'.format(script_path, latest)
 i = open(f + '.in', 'r')
 o = open(f + '.out', 'w')
 print(f)
