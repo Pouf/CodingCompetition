4afe0ab8c1583d4bf0052a610bddebb611140f7c
Benoit Delaunay <Pouf@users.noreply.github.com>
2018-09-07 13:36:44 +0200

    Update A.py

--- Google-Code-Jam/2016-Q/A.py
+++ Google-Code-Jam/2016-Q/A.py
@@ -3,20 +3,20 @@ import sys
 
 
 script = __file__
-scriptPath = os.path.dirname(script)
-scriptFile = os.path.basename(script)[0]
-files = [f for f in os.listdir(scriptPath) if scriptFile in f and '.in' in f]
-if '{}-large'.format(scriptFile) in str(files):
-  size = 'large'
-elif '{}-small'.format(scriptFile) in str(files):
-  size = 'small'
-elif '{}-test'.format(scriptFile) in str(files):
-  size = 'test'
+script_path = os.path.dirname(script)
+script_file = os.path.basename(script)[0]
+files = [f for f in os.listdir(script_path) if script_file in f and '.in' in f]
+if '{}-large'.format(script_file) in str(files):
+    size = 'large'
+elif '{}-small'.format(script_file) in str(files):
+    size = 'small'
+elif '{}-test'.format(script_file) in str(files):
+    size = 'test'
 else:
-  print('{}-test not found'.format(scriptFile))
-  sys.exit()
+    print('{}-test not found'.format(script_file))
+    sys.exit()
 latest = sorted(f for f in files if size in f)[-1][:-3]
-f = '{}/{}'.format(scriptPath, latest)
+f = '{}/{}'.format(script_path, latest)
 i = open(f + '.in', 'r')
 o = open(f + '.out', 'w')
 print(f)
@@ -29,18 +29,18 @@ import string
 
 
 for t in range(T):
-  left = set(string.digits)
-  iN = N = int(i.readline().rstrip())
-  left -= set(str(N))
-  while left:
-    iN += N
-    left -= set(str(iN))
-    if iN == N:
-      result = 'INSOMNIA'
-      break
-  else:
-    result = iN
-  o.write('Case #{}: {}{}'.format(t + 1, result, ['\n', ''][t == T - 1]))
+    left = set(string.digits)
+    iN = N = int(i.readline().rstrip())
+    left -= set(str(N))
+    while left:
+        iN += N
+        left -= set(str(iN))
+        if iN == N:
+            result = 'INSOMNIA'
+            break
+    else:
+        result = iN
+    o.write('Case #{}: {}{}'.format(t + 1, result, ['\n', ''][t == T - 1]))
 
 i.close()
 o.close()
