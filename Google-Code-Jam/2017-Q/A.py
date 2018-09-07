5fc436144dd20a29436b8b1005bc422c514df304
Benoit Delaunay <Pouf@users.noreply.github.com>
2018-09-07 13:42:36 +0200

    Update A.py

--- Google-Code-Jam/2017-Q/A.py
+++ Google-Code-Jam/2017-Q/A.py
@@ -3,20 +3,20 @@ import sys
 
 
 script = __file__
-scriptPath = os.path.dirname(script)
-scriptFile = os.path.basename(script)[0]
-files = [f for f in os.listdir(scriptPath) if scriptFile in f and '.in' in f]
-if '{}-large'.format(scriptFile) in str(files):
-  size = 'large'
-elif '{}-small'.format(scriptFile) in str(files):
-  size = 'small'
-elif '{}-test'.format(scriptFile) in str(files):
-  size = 'test'
+script_path = os.path.dirname(script)
+script_file = os.path.basename(script)[0]
+files = [f for f in os.listdir(script_path) if script_file in f and '.in' in f]
+if '{}-large'.format(script_file) in str(files):
+    size = 'large'
+elif '{}-small'.format(script_file) in str(files):
+    size = 'small'
+elif '{}-test'.format(script_file) in str(files):
+    size = 'test'
 else:
-  print('{}-test not found'.format(scriptFile))
-  sys.exit()
+    print('{}-test not found'.format(script_file))
+    sys.exit()
 latest = sorted(f for f in files if size in f)[-1][:-3]
-F = '{}/{}'.format(scriptPath, latest)
+F = '{}/{}'.format(script_path, latest)
 I = open(F + '.in', 'r')
 O = open(F + '.out', 'w')
 print(F)
@@ -29,22 +29,22 @@ T = int(I.readline())  # nb of test cases
 transtab = str.maketrans('+-', '-+')
 
 for x in range(T):
-  S, K = I.readline().rstrip().split()
-  K = int(K)
-  y = 0
-  for i in range(len(S)):
+    S, K = I.readline().rstrip().split()
+    K = int(K)
+    y = 0
+    for i in range(len(S)):
+        if '-' in S:
+            if S[i] == '-' and i + K <= len(S):
+                S = S[:i] + S[i:][:K].translate(transtab) + S[i+K:]
+                y += 1
+        else:
+            break
     if '-' in S:
-      if S[i] == '-' and i + K <= len(S):
-        S = S[:i] + S[i:][:K].translate(transtab) + S[i+K:]
-        y += 1
-    else:
-      break
-  if '-' in S:
-    y = 'IMPOSSIBLE'
+        y = 'IMPOSSIBLE'
 
-  result = '{}Case #{}: {}'.format('\n' if x else '', x + 1, y)
-  # print(result)
-  O.write(result)
+    result = '{}Case #{}: {}'.format('\n' if x else '', x + 1, y)
+    # print(result)
+    O.write(result)
 
 I.close()
 O.close()
