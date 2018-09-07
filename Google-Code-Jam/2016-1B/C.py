83b0337fe2b0a6ebfe4b7e85144e3a47c151e5c5
Benoit Delaunay <Pouf@users.noreply.github.com>
2018-09-07 13:34:15 +0200

    Update C.py

--- Google-Code-Jam/2016-1B/C.py
+++ Google-Code-Jam/2016-1B/C.py
@@ -3,20 +3,20 @@ import sys
 
 
 script = __file__
-scriptPath = os.path.dirname(script)
-scriptFile = os.path.basename(script)[0]
-files = [f for f in os.listdir(scriptPath) if scriptFile in f and '.in' in f]
-if '{}-large'.format(scriptFile) in str(files):
-  size = 'large'
-elif '{}-small'.format(scriptFile) in str(files):
-  size = 'small'
-elif '{}-test'.format(scriptFile) in str(files):
-  size = 'test'
+script_path = os.path.dirname(script)
+script_file = os.path.basename(script)[0]
+files = [f for f in os.listdir(script_path) if script_file in f and '.in' in f]
+if '{}-large'.format(script_file) in str(files):
+    size = 'large'
+elif '{}-small'.format(script_file) in str(files):
+    size = 'small'
+elif '{}-test'.format(script_file) in str(files):
+    size = 'test'
 else:
-  print('{}-test not found'.format(scriptFile))
-  sys.exit()
+    print('{}-test not found'.format(script_file))
+    sys.exit()
 latest = sorted(f for f in files if size in f)[-1][:-3]
-f = '{}/{}'.format(scriptPath, latest)
+f = '{}/{}'.format(script_path, latest)
 i = open(f + '.in', 'r')
 o = open(f + '.out', 'w')
 print(f)
@@ -26,11 +26,11 @@ T = int(i.readline())
 # Problem C. Technobabble
 
 for x in range(T):
-  N = int(i.readline())
-  S = [int(s) for l in range(2 * N - 1) for s in i.readline().rstrip().split()]
-  y = ' '.join(str(t) for t in sorted(set(s for s in S if S.count(s) % 2)))
+    N = int(i.readline())
+    S = [int(s) for l in range(2 * N - 1) for s in i.readline().rstrip().split()]
+    y = ' '.join(str(t) for t in sorted(set(s for s in S if S.count(s) % 2)))
 
-  o.write('{}Case #{}: {}'.format(['', '\n'][x > 0], x + 1, y))
+    o.write('{}Case #{}: {}'.format(['', '\n'][x > 0], x + 1, y))
 
 i.close()
 o.close()
