22c0db99e575d94064ed32beb8606a263d5052fd
Benoit Delaunay <Pouf@users.noreply.github.com>
2018-09-07 13:11:39 +0200

    Update common-words.py

--- CiO/common-words.py
+++ CiO/common-words.py
@@ -1,2 +1,2 @@
 def checkio(a, b):
-  return ','.join(x for x in sorted(a.split(',')) if x in sorted(b.split(',')))
+    return ','.join(x for x in sorted(a.split(',')) if x in sorted(b.split(',')))
