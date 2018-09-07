8159d4501384b23b77f3a6eec6b9e7a1689c763c
Benoit Delaunay <Pouf@users.noreply.github.com>
2018-09-07 13:16:21 +0200

    Update word-pattern.py

--- CiO/word-pattern.py
+++ CiO/word-pattern.py
@@ -2,6 +2,6 @@ from re import sub
 
 
 def check_command(p, c):
-  A = sub('\D','1', sub('\d','0',c))
-  B = '{:0{}b}'.format(p, len(c))
-  return A == B
+    A = sub('\D','1', sub('\d','0',c))
+    B = '{:0{}b}'.format(p, len(c))
+    return A == B
