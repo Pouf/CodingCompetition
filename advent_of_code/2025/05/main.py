63b7b3a0201906bedf6fd0df8e636e65a6369b43
Aurore Delaunay <Pouf@users.noreply.github.com>
2025-12-03 07:09:49 +0100

    Update main.py

--- advent_of_code/2025/05/main.py
+++ advent_of_code/2025/05/main.py
@@ -1,12 +1,19 @@
+import typing
+from utils import setup
 
 
-def main(data):
+def main(test_input: list[str]) -> typing.Iterator[typing.Any]:
     result = 0
-    for line in data:
+    for line in test_input:
         batteries = list(line)
         best = max(batteries[:-1]
         position = batteries.index(best)
         remaining = vatteries[position:]
         next_best = max(remaining)
         result += int(f"{best}{next_best}")
+  
     yield result
+
+
+if __name__ == "__main__":
+    setup(main)
