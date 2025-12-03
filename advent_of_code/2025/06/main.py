8ef213682bd605a67fbc6133f9d89bc8a1acb858
Aurore Delaunay <aurore.delaunay@forticheprod.com>
2025-12-03 10:20:37 +0100

    advent puzzle 6

--- advent_of_code/2025/06/main.py
+++ advent_of_code/2025/06/main.py
@@ -5,11 +5,12 @@ from utils import setup
 def main(test_input: list[str]) -> typing.Iterator[typing.Any]:
     result = 0
     for bank in test_input:
-        best = max(bank[:-1])
-        position = bank.index(best)
-        bank = bank[position + 1:]
-        next_best = max(bank)
-        joltage = f"{best}{next_best}"
+        joltage = ""
+        for n in range(12):
+            best = max(bank[:-12 + n + 1 or None])
+            position = bank.index(best)
+            bank = bank[position+1:]
+            joltage += best
         result += int(joltage)
   
     yield result
