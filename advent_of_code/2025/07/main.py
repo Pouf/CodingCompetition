a4a749b1b80ff88a9400f042775af4ad34969da1
Aurore Delaunay <aurore.delaunay@forticheprod.com>
2025-12-04 16:21:38 +0100

    advent puzzle 7 and 8

---
+++ advent_of_code/2025/07/main.py
@@ -0,0 +1,24 @@
+import typing
+from utils import setup
+
+
+def main(test_input: list[str]) -> typing.Iterator[typing.Any]:
+    DIRECTIONS = (1, -1, 1j, -1j, 1+1j, 1-1j, -1+1j, -1-1j)
+    result = 0
+    floor_plan = dict()
+    for x, row in enumerate(test_input):
+        for y, symbol in enumerate(row):
+            floor_plan[x+y*1j] = symbol == "@"
+    for coords, value in floor_plan.items():
+        if not value:
+            continue
+        nb_rolls_around = sum(
+            floor_plan.get(coords + direction, 0)
+            for direction in DIRECTIONS)
+        result += nb_rolls_around < 4
+  
+    yield result
+
+
+if __name__ == "__main__":
+    setup(main)
---
+++ advent_of_code/2025/08/main.py
@@ -0,0 +1,31 @@
+import typing
+from utils import setup
+
+
+def main(test_input: list[str]) -> typing.Iterator[typing.Any]:
+    DIRECTIONS = (1, -1, 1j, -1j, 1+1j, 1-1j, -1+1j, -1-1j)
+    result = 0
+    floor_plan = dict()
+    for x, row in enumerate(test_input):
+        for y, symbol in enumerate(row):
+            floor_plan[x+y*1j] = symbol == "@"
+
+    modified = True
+    while modified:
+        modified = False
+        for coords, value in floor_plan.items():
+            if not value:
+                continue
+            nb_rolls_around = sum(
+                floor_plan.get(coords + direction, 0)
+                for direction in DIRECTIONS)
+            if nb_rolls_around < 4:
+                floor_plan[coords] = 0
+                result += 1
+                modified = True
+  
+    yield result
+
+
+if __name__ == "__main__":
+    setup(main)
