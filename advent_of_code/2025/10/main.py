cf06bd19dc8453aa51eeb05504518780287fe3db
Aurore Delaunay <aurore.delaunay@forticheprod.com>
2025-12-05 13:56:07 +0100

    style: simplify

--- advent_of_code/2025/10/main.py
+++ advent_of_code/2025/10/main.py
@@ -1,33 +1,29 @@
 import typing
 from utils import setup
-from collections import deque
 
 
 def main(test_input: list[str]) -> typing.Iterator[typing.Any]:
     result = 0
+
     ranges = set()
     for line in test_input:
         try:
             start, stop = map(int, line.split("-"))
             ranges.add((start, stop + 1))
-        except ValueError:
+        except ValueError:  # Empty line
             break
 
-    sorted_ranges = deque(sorted(ranges))
+    sorted_ranges = sorted(ranges, reverse=True)
     while sorted_ranges:
-        start, stop = sorted_ranges.popleft()
-        # print(f"==>> {start}-{stop}")
+        start, stop = sorted_ranges.pop()
         while sorted_ranges:
-            next_start, next_stop = sorted_ranges[0]
-            # print(f"==>> next {next_start}-{next_stop} {next_start <= stop}")
+            next_start, next_stop = sorted_ranges[-1]
             if next_start <= stop:
                 stop = max(stop, next_stop)
-                sorted_ranges.popleft()
+                sorted_ranges.pop()
             else:
                 break
-        # print(f"==>> stop - start: {stop - start}")
         result += stop - start
-    # print(f"==>> result: {result}")
 
     yield result
 
