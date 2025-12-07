3dcabcc829797cae73725946bf8df60ebe928b2b
Aurore Delaunay <Pouf@users.noreply.github.com>
2025-12-07 07:24:49 +0100

    Create main.py

---
+++ advent_of_code/2025/11/main.py
@@ -0,0 +1,32 @@
+from typing import Iterator, Any
+from utils import setup
+
+
+def main(test_input: Iterator[str]) -> Iterator[Any]:
+    result = 0
+
+    ranges = set()
+    for line in test_input:
+        try:
+            start, stop = map(int, line.split("-"))
+            ranges.add((start, stop + 1))
+        except ValueError:  # Empty line
+            break
+
+    sorted_ranges = sorted(ranges, reverse=True)
+    while sorted_ranges:
+        start, stop = sorted_ranges.pop()
+        while sorted_ranges:
+            next_start, next_stop = sorted_ranges[-1]
+            if next_start <= stop:
+                stop = max(stop, next_stop)
+                sorted_ranges.pop()
+            else:
+                break
+        result += stop - start
+
+    yield result
+
+
+if __name__ == "__main__":
+    setup(main)
