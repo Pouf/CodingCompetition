2d14615c91cee96be9c8a919497c1aa06cf6bc4c
Aurore Delaunay <Pouf@users.noreply.github.com>
2025-12-08 22:43:15 +0100

    Update main.py

--- advent_of_code/2025/13/main.py
+++ advent_of_code/2025/13/main.py
@@ -3,22 +3,31 @@ from utils import setup
 
 
 def main(test_input: Iterator[str]) -> Iterator[Any]:
-    result = 0
-
-    DIRECTIONS = (
-        -1-1j, -1, -1+1j,
-          -1j,       +1j,
-        +1-1j, +1, +1+1j,
-    )
-    result = 0
+    splits = 0
+    
+    # -1-1j  -1  -1+1j
+    #   -1j   X    +1j
+    # +1-1j  +1  +1+1j
+    stack = set()
     floor_plan = dict()
     for x, row in enumerate(test_input):
         for y, symbol in enumerate(row):
-            floor_plan[x+y*1j] = symbol == "@"
-
+            floor_plan[x+y*1j] = symbol
+            if symbol == "S":
+                stack.add(x+1 + y*1j})
     
-    yield result
+    while stack:
+        current = stack.pop()
+        visited.add(current)
+        if floor_plan[current] == ".":
+            down = current + 1
+            stack.add(down)
+        next = floor_plan[down]
+        if next == "^":
+            left = current
+        stack -= visited
 
+    yield splits
 
 
 if __name__ == "__main__":
