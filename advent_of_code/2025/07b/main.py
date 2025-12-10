f62dbbac82220ec8cfaec9ffe3f1fc66fdaeaa35
Aurore Delaunay <Pouf@users.noreply.github.com>
2025-12-10 08:41:37 +0100

    Update main.py

--- advent_of_code/2025/07b/main.py
+++ advent_of_code/2025/07b/main.py
@@ -3,42 +3,40 @@ from utils import setup
 
 
 def main(test_input: Iterator[str]) -> Iterator[Any]:
-    """
-    Directions:
-    -1-1j  -1  -1+1j
-      -1j   X    +1j
-    +1-1j  +1  +1+1j
-    """
     result = 0
-    floor_plan = dict()
-    stack: list[complex] = []
 
-    # Building nodes
+    # Building DAG
+    test_input = list(test_input)
     for x, row in enumerate(test_input):
         if x % 2:
             continue  # Skip useless empty rows
         for y, symbol in enumerate(row):
-            coords = x//2 + y*1j
-            floor_plan[coords] = symbol
             if symbol == "S":
-                stack.append(coords)
+                start = (x, y)
+
+    visited: set[complex] = set()
+    stack = set(start)
 
     # DAG traversal
     while stack:
         current = stack.pop()
-        down = current + 1
-        symbol_below = floor_plan.get(down)
-        if symbol_below in ("S", "."):
-            stack.append(down)
-        elif symbol_below == "^":
-            down_left = down - 1j
-            down_right = down + 1j
-            stack += [down_left, down_right]
+        neighbors = get_neighbors
         elif symbol_below is None:
             result += 1
 
     yield result
 
+def get_neighbors(coords, nodes):
+    x, y = coords
+    down = 
+    symbol_below = floor_plan.get(down)
+    if symbol_below in ("S", "."):
+            stack.append(down)
+        elif symbol_below == "^":
+            down_left = down - 1j
+            down_right = down + 1j
+            stack += [down_left, down_right]
+    
 
 if __name__ == "__main__":
     setup(main)
