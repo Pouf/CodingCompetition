98e1e3af8cb0a0abed1cc1745ef3470c70e9de48
Aurore Delaunay <aurore.delaunay@forticheprod.com>
2025-12-10 09:19:00 +0100

    advent 7b

--- advent_of_code/2025/07b/main.py
+++ advent_of_code/2025/07b/main.py
@@ -1,42 +1,41 @@
+from functools import cache
 from typing import Iterator, Any
 from utils import setup
 
 
 def main(test_input: Iterator[str]) -> Iterator[Any]:
-    result = 0
+    nodes = tuple(test_input)
+    start = (0, nodes[0].index("S"))
+    yield get_nb_paths(start, nodes)
 
-    # Building DAG
-    test_input = list(test_input)
-    for x, row in enumerate(test_input):
-        if x % 2:
-            continue  # Skip useless empty rows
-        for y, symbol in enumerate(row):
-            if symbol == "S":
-                start = (x, y)
 
-    visited: set[complex] = set()
-    stack = set(start)
+@cache
+def get_nb_paths(start, nodes: tuple[str, ...]) -> int:
+    neighbors = get_neighbors(start, nodes)
+    if not neighbors:
+        return 1
+    return sum(
+        get_nb_paths(node, nodes)
+        for node in neighbors
+    )
 
-    # DAG traversal
-    while stack:
-        current = stack.pop()
-        neighbors = get_neighbors
-        elif symbol_below is None:
-            result += 1
 
-    yield result
+def get_neighbors(start: tuple[int, int], nodes: tuple[str, ...]) -> set[tuple[int, int]]:
+    x, y = start
+    try:
+        symbol_below = nodes[x + 2][y]
+    except IndexError:
+        return set()
 
-def get_neighbors(coords, nodes):
-    x, y = coords
-    down = 
-    symbol_below = floor_plan.get(down)
-    if symbol_below in ("S", "."):
-            stack.append(down)
-        elif symbol_below == "^":
-            down_left = down - 1j
-            down_right = down + 1j
-            stack += [down_left, down_right]
-    
+    if symbol_below == "^":
+        down_left = (x + 2, y - 1)
+        down_right = (x + 2, y + 1)
+        return {down_left, down_right}
+
+    else:  # `S` or `.`
+        down = (x + 2, y)
+        return {down}
+
 
 if __name__ == "__main__":
     setup(main)
