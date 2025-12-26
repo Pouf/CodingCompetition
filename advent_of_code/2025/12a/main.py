10408ca60ad584ae8c47c43469466ce906b76cb4
Aurore Delaunay <aurore.delaunay@forticheprod.com>
2025-12-26 13:18:08 +0100

    advent 12

--- advent_of_code/2025/12a/main.py
+++ advent_of_code/2025/12a/main.py
@@ -1,11 +1,15 @@
+import re
 from typing import Iterator, Any
 from utils import setup
 
 
 def main(test_input: list[str]) -> Iterator[Any]:
     result = 0
-    while test_input:
-        result += 1
+
+    for line in test_input:
+        if "x" in line:
+            width, height, *presents = map(int, re.findall(r"\d+", line))
+            result += (width // 3 * height // 3 >= sum(presents))
     yield result
 
 
