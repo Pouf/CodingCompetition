f1f5455e905bdd5e8f72daa0fc88027dfed2e7da
Aurore Delaunay <Pouf@users.noreply.github.com>
2025-12-26 06:20:58 +0100

    advent_11a: Implement pathfinding logic in main function
    Refactor main function to track edges and find complete paths.

--- advent_of_code/2025/11a/main.py
+++ advent_of_code/2025/11a/main.py
@@ -3,10 +3,25 @@ from typing import Iterator, Any
 
 
 def main(test_input: list[str]) -> Iterator[Any]:
-    result = 0
+    edges = dict()
     for line in test_input:
-        result += 1
-    yield result
+        node, value = line.split(": ")
+        edges[node] = frozenset(value.split())
+
+    stack = {"you"}
+    complete_paths = set()
+    while stack:
+        path = stack.pop()
+        node = path[-3:]
+        for new_node in edges[node]:
+            if new_node in path:
+                continue
+            new_path = f"{path}.{new_node}"
+            if new_node == "out":
+                print(path)
+                complete_paths.add(path)
+            stack.add(path)
+    yield len(complete_paths)
 
 
 if __name__ == "__main__":
